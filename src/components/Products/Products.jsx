5ee3a4dac8f3c610b6b2e23c47a695401f1c28d6
Jadson Santos <jadsondossantos5@gmail.com>
2023-09-05 17:36:06 -0300

    loading

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,18 +2,21 @@ import './Products.css'
 
 import React from 'react'
 
+import Loading from '../Loading/Loading'
 import ProductCard from '../ProductCard/ProductCard'
 import useProducts from './useProducts'
 
 const Products = () => {
-  const { products } = useProducts()
+  const { products, loading } = useProducts()
 
   return (
-    <section className="products container">
-      {products.map((product) => (
-        <ProductCard key={product.id} data={product} />
-      ))}
-    </section>
+    (loading && <Loading />) || (
+      <section className="products container">
+        {products.map((product) => (
+          <ProductCard key={product.id} data={product} />
+        ))}
+      </section>
+    )
   )
 }
 
--- src/components/Products/useProducts.js
+++ src/components/Products/useProducts.js
@@ -4,14 +4,16 @@ import fetchProducts from '../../api/fetchProducts'
 
 const useProducts = () => {
   const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     fetchProducts('celular').then((response) => {
       setProducts(response)
+      setLoading(false)
     })
   }, [])
 
-  return { products, setProducts }
+  return { products, loading }
 }
 
 export default useProducts
