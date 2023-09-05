ad90950fe95b3233c8495f75a980802563492880
Jadson Santos <jadsondossantos5@gmail.com>
2023-09-05 17:05:48 -0300

    import product component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import './App.css'
 import React from 'react'
 
 import Header from './components/Header/Header'
+import Products from './components/Products/Products'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Products />
     </div>
   )
 }
